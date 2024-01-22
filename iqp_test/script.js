c29836b4c2cd55ebf904846977ec4f59b3826675
JD <mac@macs-MacBook-Pro-2.local>
2024-01-21 20:23:57 -0400

    polygon grid test

--- iqp_test/script.js
+++ iqp_test/script.js
@@ -1,11 +1,59 @@
-var map = L.map('map').setView([18.4518, -66.0597], 16);
+
+//size of grid tiles
+var gridSize = .00018;
+
+//42 * 85 grid
+var gridRows = 43;
+var gridCols = 86;
+
+var map = L.map('map');
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
+    minZoom: 16,
+    maxBoundsViscosity: 1.0,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
 
+//bounds of monitoring area
+var lat1 = 18.454677;
+var lat2 = lat1 - gridRows*gridSize;
+var long1 = -66.06622;
+var long2 = long1 + gridCols*gridSize;
+var bounds = [[lat1, long1],[lat2, long2]]
+
+var boundRect = L.rectangle(bounds, {
+    color: 'red',
+    fill: false,
+    }).addTo(map);
+
+map.fitBounds(bounds);
+map.setMaxBounds(bounds);
+
+
+//polygon grid
+for (var i = 0; i < gridRows; i++) {
+    for (var j = 0; j < gridCols; j++) {
+        var rect = L.rectangle([[lat1-gridSize*(i), long1+gridSize*(j)],[lat1-gridSize*(i+1), long1+gridSize*(j+1)]], {
+            color: "#000000",
+            opacity: 0.1,
+            fillColor: "#3388ff",
+            fillOpacity: 0.3,
+            weight: 1,
+            }).addTo(map);
+        rect.bindPopup("Grid tile r" + i + "c" + j);
+    }
+}
+
+function onMapClick(e) {
+    alert("You clicked the map at " + e.latlng);
+}
+
+map.on('click', onMapClick);
+
+//GRIDLAYER - old method, switching to grid of polgons but keeping this here in case
+/*
 L.GridLayer.LocGrid = L.GridLayer.extend({
     createTile: function (coords) {
         var tile = document.createElement('div');
@@ -28,8 +76,10 @@ L.gridLayer.locGrid = function(opts) {
 };
 
 map.addLayer( L.gridLayer.locGrid() );
+*/
 
 
+// INPUT STUFF
 
 var slider = document.getElementById("perception");
 var output = document.getElementById("perception-num");
