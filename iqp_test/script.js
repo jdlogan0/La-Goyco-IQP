6d51aa5bc165e6bed359c0d4f492a5561d2080ff
JD <mac@macs-MacBook-Pro-2.local>
2024-01-24 17:02:05 -0400

    still trying to fix React

--- iqp_test/script.js
+++ iqp_test/script.js
@@ -32,37 +32,17 @@ map.fitBounds(bounds);
 map.setMaxBounds(bounds);
 
 
-
 //polygon grid
 for (var i = 0; i < gridRows; i++) {
     for (var j = 0; j < gridCols; j++) {
-        var tileName = "r" + i + "c" + j;
         var rect = L.rectangle([[lat1-gridSize*(i), long1+gridSize*(j)],[lat1-gridSize*(i+1), long1+gridSize*(j+1)]], {
             color: "#000000",
             opacity: 0.1,
             fillColor: "#3388ff",
             fillOpacity: 0.3,
             weight: 1,
-            name: tileName
             }).addTo(map);
-        rect.bindPopup("Grid tile " + tileName);
-        rect.on('click', function(e) {
-            showData(this.options.name);
-        });
-        rect.on("mouseover",function(e) {
-            this.setStyle({
-                color: 'white',
-                opacity: 1.0,
-                weight: 2,
-              });
-        });
-        rect.on("mouseout",function(e) {
-            this.setStyle({
-                color: '#000000',
-                opacity: 0.1,
-                weight: 1,
-              });
-          });
+        rect.bindPopup("Grid tile r" + i + "c" + j);
     }
 }
 
@@ -72,82 +52,51 @@ function onMapClick(e) {
 
 map.on('click', onMapClick);
 
+//GRIDLAYER - old method, switching to grid of polgons but keeping this here in case
+/*
+L.GridLayer.LocGrid = L.GridLayer.extend({
+    createTile: function (coords) {
+        var tile = document.createElement('div');
+        //tile.innerHTML = [coords.x, coords.y, coords.z].join(', ');
+        tile.style.border = '1px solid #00000022';
+        //tile.style.backgroundColor = '#ff00004a';
+        tile.style.boxSizing = "border-box"
+        tile.style.padding = "5px"
+        //tile.width = size.x;
+        //tile.height = size.y;
+        return tile;
+    },
+    options: {
+		tileSize: 64,
+    }
+});
+
+L.gridLayer.locGrid = function(opts) {
+    return new L.GridLayer.LocGrid(opts);
+};
+
+map.addLayer( L.gridLayer.locGrid() );
+*/
+
 
 // INPUT STUFF
 
-var backData = document.getElementById("back-data");
-var backReport = document.getElementById("back-report");
+var slider = document.getElementById("perception");
+var output = document.getElementById("perception-num");
+output.innerHTML = slider.value; 
+slider.oninput = function() {
+    output.innerHTML = this.value;
+}
+
+var back = document.getElementById("back");
 var info = document.getElementById("map-info");
 var report = document.getElementById("map-report");
-var data = document.getElementById("map-data");
-var tName = document.getElementById("tile-name");
 var reportbtn = document.getElementById("reportBtn");
-backData.onclick = function() {
-    info.style.display = "block";
-    data.style.display = "none";
-}
-backReport.onclick = function() {
+back.onclick = function() {
     info.style.display = "block";
     report.style.display = "none";
 }
-
 reportbtn.onclick = function() {
     info.style.display = "none";
-    data.style.display = "none";
     report.style.display = "block";
-}
-
-function showData(tile) {
-    tName.innerHTML = "Grid tile " + tile;
-    info.style.display = "none";
-    report.style.display = "none";
-    data.style.display = "block";
-}
-
-const loudDesc = ["No noise at all","1 desc","2 desc","3 desc","4 desc","pleasant i guess?", "6 desc", "7 desc", "8 desc", "9 desc", "Sitting next to a jet taking off"];
-const loudRange = document.getElementById("perception");
-const loudTxt = document.getElementById("loudTxt");
-var loudCurrent = document.getElementById("perception-num");
-loudCurrent.innerHTML = loudRange.value; 
-loudRange.oninput = function(e) {
-    loudTxt.innerHTML = loudDesc[e.target.value];
-    loudCurrent.innerHTML = this.value;
-  };
-
-
-const emojis = ['😀','🙂','😐','🙁','😢'];
-const feelingRange = document.getElementById("feeling");
-const emoji = document.getElementById("emoji");
-feelingRange.oninput = function(e) {
-    emoji.innerHTML = emojis[e.target.value];
-  };
-
-
-//tag stuff
-function hideTags() {
-    document.getElementById("tagSearch").value = "";
-    document.getElementById("tag-dropdown").style.display = "none";
-}
-  
-function filterTags() {
-    var input, filter, ul, li, a, i;
-    var input = document.getElementById("tagSearch");
-    var filter = input.value.toUpperCase();
-    var dropdown = document.getElementById("tag-dropdown");
-    var tags = dropdown.getElementsByTagName("button");
-    var totalShown = 0;
-    for (i = 0; i < tags.length; i++) {
-      tagName = tags[i].innerText;
-      if ((tagName.toUpperCase().indexOf(filter) > -1) && (totalShown < 5)){
-        tags[i].style.display = "";
-        totalShown += 1;
-      } else {
-        tags[i].style.display = "none";
-      }
-    }
-    dropdown.style.display = "block";
-}
-
-function addTag(tag) {
-    
 }
